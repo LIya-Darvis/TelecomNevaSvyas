2f87a196296038c5e9f692953ced63eddc1a9cd2
Liya Darvis <anastasiaadm2019@gmail.com>
2023-02-24 11:54:39 +0300

    Autrhorisation creating

---
+++ TelecomNevaSvyas/AuthorisationWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace TelecomNevaSvyas
+{
+    public partial class AuthorisationWindow : Window
+    {
+        public AuthorisationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ClearTextBoxes(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void GoToMain(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
+    }
+}
