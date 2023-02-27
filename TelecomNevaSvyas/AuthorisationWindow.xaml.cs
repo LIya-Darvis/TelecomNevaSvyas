f0dc4c45874863c01bdf02137e340ba10883742f
Liya Darvis <anastasiaadm2019@gmail.com>
2023-02-27 11:59:20 +0300

    Number Validation

--- TelecomNevaSvyas/AuthorisationWindow.xaml.cs
+++ TelecomNevaSvyas/AuthorisationWindow.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 using Npgsql;
+using TelecomNevaSvyas.DBModels;
 
 namespace TelecomNevaSvyas
 {
@@ -14,31 +18,61 @@ namespace TelecomNevaSvyas
 
         private void ClearTextBoxes(object sender, RoutedEventArgs e)
         {
-            throw new System.NotImplementedException();
+            NumberTextBox.Clear();
+            PasswordTextBox.Clear();
+            CodeTextBox.Clear();
+        }
+        
+        private void NumberValidation(object sender, KeyEventArgs e)
+        {
+            String number = NumberTextBox.Text;
+            
+            if (e.Key == Key.Return)
+            {
+                EmployeesModel employeesModel = new EmployeesModel();
+                bool info = employeesModel.NumberInDB(number);
+
+                if (info)
+                {
+                    IntoButton.IsEnabled = true;
+                    PasswordTextBox.IsEnabled = true;
+                    PasswordTextBox.IsFocused.Equals(true);
+                }
+                else
+                {
+                    IntoButton.IsEnabled = false;
+                    PasswordTextBox.IsEnabled = false;
+                    PasswordTextBox.IsFocused.Equals(false);
+                    MessageBox.Show("Номер отсутствует в базе", "Ошибка");
+                }
+            }
+            
+            
         }
 
         private void GoToMain(object sender, RoutedEventArgs e)
         {
-            // EmployeesModel testConnection = new EmployeesModel();
-            // testConnection.TestConnection();
             
-            Console.WriteLine("Переход");
-            
-            MainWindow mainWindow = new MainWindow();
-            mainWindow.Show();
-            
-            NpgsqlConnection conn = new NpgsqlConnection("Server=localhost;User Id=postgres;Password=123;Database=TelecomNevaSvyasDB;");
-            conn.Open();
- 
-            // Define a query returning a single row result set
-            NpgsqlCommand command = new NpgsqlCommand("SELECT COUNT(*) FROM Positions", conn);
- 
-            // Execute the query and obtain the value of the first column of the first row
-            Int64 count = (Int64)command.ExecuteScalar();
- 
-            Console.Write("{0}\n", count);
-            var connectionLabelContent = mainWindow.ConnectionLabel.Content = (count);
-        }
 
+            MainWindow mainWindow = new MainWindow();
+            // var connectionLabelContent = mainWindow.ConnectionLabel.Content = (info);
+
+            // if (info)
+            // {
+            mainWindow.Show();
+            // }
+
+
+            // NpgsqlConnection conn = new NpgsqlConnection("Server=localhost;User Id=postgres;Password=123;Database=TelecomNevaSvyasDB;");
+            // conn.Open();
+            //
+            // // Define a query returning a single row result set
+            // NpgsqlCommand command = new NpgsqlCommand("SELECT name FROM Positions", conn);
+            //
+            // // Execute the query and obtain the value of the first column of the first row
+            // String count = command.ExecuteScalar().ToString();
+            //
+            // var connectionLabelContent = mainWindow.ConnectionLabel.Content = (count);
+        }
     }
 }
---
+++ TelecomNevaSvyas/DBModels/DBConnection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Npgsql;
+
+namespace TelecomNevaSvyas.DBModels;
+
+public class DBConnection
+{
+    public NpgsqlConnection Connection()
+    {
+        NpgsqlConnection conn = new NpgsqlConnection("Server=localhost;User Id=postgres;Password=123;Database=TelecomNevaSvyasDB;");
+        conn.Open();
+
+        return conn;
+    }
+}
