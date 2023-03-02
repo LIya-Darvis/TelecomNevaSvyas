98e7ccd6175566ce257c2069db1eddaf9caf683c
Liya Darvis <anastasiaadm2019@gmail.com>
2023-03-02 15:17:05 +0300

    Captcha

---
+++ TelecomNevaSvyas/Code.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace TelecomNevaSvyas;
+
+public partial class Code : Window
+{
+    public Code()
+    {
+        InitializeComponent();
+    }
+
+    private void Code_OnClosing(object? sender, CancelEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+}
