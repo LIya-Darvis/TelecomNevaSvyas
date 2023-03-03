fa5f27f2772413b65f6230be6c532aef58c00ea0
Liya Darvis <anastasiaadm2019@gmail.com>
2023-03-03 10:06:20 +0300

    Timer

--- TelecomNevaSvyas/CodeWindow.xaml.cs
+++ TelecomNevaSvyas/CodeWindow.xaml.cs
@@ -17,22 +17,33 @@ public partial class CodeWindow : Window
 
     private void Code_OnClosing(object? sender, CancelEventArgs e)
     {
-        // DispatcherTimer dispatcherTimer = new DispatcherTimer();
-        // dispatcherTimer.Interval = TimeSpan.FromSeconds(1);
-        // dispatcherTimer.Tick += dispatcherTimerTicker;
-        // dispatcherTimer.Start();
+        DispatcherTimer dispatcherTimer = new DispatcherTimer();
+        dispatcherTimer.Interval = TimeSpan.FromSeconds(1);
+        dispatcherTimer.Tick += dispatcherTimerTicker;
+        dispatcherTimer.Start();
     }
 
     public int count = 10;
     private void dispatcherTimerTicker(object sender, EventArgs e)
     {
         count--;
-        // authorisationWindow.ValidationLabel.Content = count.ToString();
-        CodeLabel.Content = count.ToString();
+        if (count == 0)
+        {
+            dispatcherTimer.Stop();
+        }
+        else
+        {
+            authorisationWindow.ValidationLabel.Visibility = Visibility.Visible;
+            authorisationWindow.ValidationLabel.Content = count.ToString();
+            CodeLabel.Content = count.ToString();
+        }
     }
 
     private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
     {
+        authorisationWindow.ValidationLabel.Visibility = Visibility.Visible;
+        authorisationWindow.ValidationLabel.Content = count.ToString();
+        Close();
         DispatcherTimer dispatcherTimer = new DispatcherTimer();
         dispatcherTimer.Interval = TimeSpan.FromSeconds(1);
         dispatcherTimer.Tick += dispatcherTimerTicker;
